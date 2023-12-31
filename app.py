22f9d5dd311e9e4d3e8a5106afef939c0d354cef
Brian Nkhata <bnkhata@mra.mw>
2023-12-31 19:45:41 +0200

    get a single job route

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 from flask import Flask, jsonify, render_template
 from database import engine
-from sqlalchemy import text
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
+def home():
     jobs = load_jobs_from_db()
-    return render_template("home.html",jobs=jobs)
+    return render_template("home.html", jobs=jobs)
+
 
 @app.route('/api/jobs')
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
 
+
+@app.route('/job/<id>')
+def show_job(id):
+    job = load_job_from_db(id)
+    return jsonify(job)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True,use_reloader=True)
+    app.run(host='0.0.0.0', debug=True, use_reloader=True)
--- database.py
+++ database.py
@@ -1,8 +1,9 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
 engine = create_engine('mysql://root:@localhost/jobhub')
 conn = engine.connect()
-   
+
+
 def load_jobs_from_db():
     try:
         with engine.connect() as conn:
@@ -12,4 +13,18 @@ def load_jobs_from_db():
     except SQLAlchemyError as e:
         print(f"Error loading jobs from the database: {e}")
         return []
+
+
+def load_job_from_db(id):
+    try:
+        with engine.connect() as conn:
+            query = text("SELECT * FROM jobs WHERE id = :id")
+            result = conn.execute(query.bindparams(id=id))
+            job = [dict(row._asdict()) for row in result.fetchall()]
+        return job
+    except SQLAlchemyError as e:
+        print(f"Error loading job from the database: {e}")
+        return None
+
+
 conn.close()
